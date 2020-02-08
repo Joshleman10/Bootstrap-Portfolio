b0c5d1a621e67e4a237ba9214960fc158ffccf84
Josh Eshleman <eshlemanjosh@gmail.com>
2020-02-07 21:46:13 -0500

    updated portfolio format

--- assets/portfolio.js
+++
@@ -1,8 +0,0 @@
-function sendMail() {
-    var link = "mailto:Eshlemanjosh@gmail.com"
-             + "&subject=" + encodeURIComponent("This is my subject")
-             + "&body=" + encodeURIComponent(document.getElementById('message').value)
-    ;
-
-    window.location.href = link;
-}
